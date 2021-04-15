696c1ddd97daf3dca5520507d8227162398bf265
kazu <g.kazuki0715@gmail.com>
2021-04-15 21:50:07 +0900

    ペジネーションの実装 #38

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use SebastianBergmann\Environment\Console;
@@ -14,6 +15,11 @@ class Post extends Model
         'file_path',
         'text'
     ];
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return  $date->format('Y-m-d H:i');
+    }
 
     /**
      * リレーションの設定
