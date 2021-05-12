59447595f3e1589d22c1758abcbc8dd20ebf3f9e
kazu <g.kazuki0715@gmail.com>
2021-05-12 23:27:23 +0900

    テストデプロイ用に変更 #93

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\CategoriesTableSeeder;
+use Database\Seeders\UsersTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategoriesTableSeeder::class,
+            UsersTableSeeder::class,
         ]);
     }
 }
