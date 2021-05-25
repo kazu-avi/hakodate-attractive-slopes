4afe39b30f675e97c2ebd3d7712507ca841f6cae
kazu <g.kazuki0715@gmail.com>
2021-05-25 19:42:32 +0900

    カテゴリ画像・コメントの修正 #120

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CategoriesTableSeeder::class,
+            CategoriesNewTableSeeder::class,
             UsersTableSeeder::class,
         ]);
     }
